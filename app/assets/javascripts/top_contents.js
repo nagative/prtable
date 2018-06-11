d58acb783a1d867124f03a942ceb6f03211185cd
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2018-06-11 20:51:21 +0900

    top-contents_slick機能の実装

---
+++ app/assets/javascripts/top_contents.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow: '<div class="slider-arrow slider-prev fa fa-angle-left"></div>',
+    nextArrow: '<div class="slider-arrow slider-next fa fa-angle-right"></div>'
+  });
+});
+
+
