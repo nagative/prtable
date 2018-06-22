06f1cbff47046e36cdd80ec9b77d7c2c5e656175
田中史彬 <tanakafumiaki@tanakafumiakiranoMacBook-puro.local>
2018-06-22 15:15:00 +0900

    ルート修正
    タグのshowページへのルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  get   'tags/:id'   =>  'tags#show'
   resources :users, only: [:index]
   resources :stories, only: [:index, :new, :create] do
   resources :tags, only: [:index, :show]
