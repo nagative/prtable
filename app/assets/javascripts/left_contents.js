5d7967b9faa7cc0386e61647e0e4d6d2eaf04c34
Yuusuke Nagata <yuusukenagata@YuusukesMacBook-Pro.local>
2018-06-23 16:54:28 +0900

    update_master_file

---
+++ app/assets/javascripts/left_contents.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<div class="slick-prev slick-arrow"></div>',
+    nextArrow: '<div class="slick-next slick-arrow"></div>'
+  });
+});
---
+++ app/assets/javascripts/tags_box.js
@@ -0,0 +1,5 @@
+$(document).on('turbolinks:load', function(){
+ $('#main-trigger').on('click', function(){
+   $('#tags_box').fadeToggle();
+ });
+});
