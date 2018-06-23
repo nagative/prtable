342a692c3ec7980401e0a92ef5a2e9cc74a37627
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2018-06-23 18:45:05 +0900

    ストーリー作成ボタン

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,23 @@
 class StoriesController < ApplicationController
   def new
+    @story = Story.new
   end
 
   def create
+    @story = @company.stories.new(story_params)
+    @story.save
+  end
+
+  def story_params
+    params.require(:story).permit(
+      :title,
+      :sub_title,
+      :user,
+      :logo,
+      :read,
+      :picture,
+      :description,
+      :contnet
+      )
   end
 end
