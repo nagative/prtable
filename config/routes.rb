bf723f62d6eced3e2a8134501c360e5b96da29f8
Yuusuke Nagata <1y5u1s7u5k7e@gmail.com>
2019-03-31 14:39:49 +0900

    devise_viewpath_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root "users#index"
   resources :users, only: [:index]
   resources :stories, only: [:index, :new, :create] do
-  resources :tags, only: [:index, :show]
+    resources :tags, only: [:index, :show]
   end
 end
